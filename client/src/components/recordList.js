ebd9fcc64edf2b146d616c1f5c58459773d9133e
Devin <devin.m.bushey@gmail.com>
2021-12-26 10:13:28 -0800

    added links to table

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 // This will require to npm install axios
 import axios from 'axios';
-import { Link } from "react-router-dom";
-
-
-import * as $ from "jquery";
-import Player from "./Player";
 import hash from "./hash";
 
 //TODO - Store in .config file
@@ -17,15 +12,14 @@ const scopes = [
   "user-read-playback-state",
 ];
 
-
 const Record = (props) => (
   <tr>
     <td>{props.record.ticket_month}</td>
     <td>{props.record.ticket_day}</td>
     <td>{props.record.ticket_band}</td>
     <td>{props.record.ticket_price}</td>
-    <td>{props.record.ticket_soldout}</td>
-
+    {/* <td>{props.record.ticket_soldout}</td> */}
+    <td><a href={props.record.link} target="_blank">spotify</a></td>
   </tr>
 );
 
@@ -36,52 +30,13 @@ export default class RecordList extends Component {
     this.state = {
       records: [],
       token: null,
-      item: {
-        album: {
-          images: [{ url: "" }]
-        },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms: 0
-      },
-      is_playing: "Paused",
-      progress_ms: 0,
-      no_data: false,
+      showTable: false,
     }
 
-    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-  }
-
-  getCurrentlyPlaying(token) {
-    // Make a call using the token
-    $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
-      beforeSend: xhr => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-      },
-      success: data => {
-        // Checks if the data is not empty
-        if(!data) {
-          this.setState({
-            no_data: true,
-          });
-          return;
-        }
-
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          progress_ms: data.progress_ms,
-          no_data: false /* We need to "reset" the boolean, in case the
-                            user does not give F5 and has opened his Spotify. */
-        });
-      }
-    });
   }
 
   // This method will get the data from the database.
-  componentDidMount() {
+  async componentDidMount() {
 
     //Spotify
     let _token = hash.access_token;
@@ -90,30 +45,31 @@ export default class RecordList extends Component {
       this.setState({
         token: _token
       });
-      this.getCurrentlyPlaying(_token);
     }
 
     axios
       .get("http://localhost:5000/record/")
       .then((response) => {
-        this.setState({ records: response.data });
+        this.setState({ records : response.data})
       })
       .catch(function (error) {
-
         console.log(error);
       });
+
   }
 
   // This method will map out the users on the table
   recordList() {
-    return this.state.records.map((currentrecord) => {
-      return (
-        <Record
-          record={currentrecord}
-          key={currentrecord._id}
-        />
-      );
-    });
+
+      return this.state.records.map((currentrecord) => {
+        return (
+          <Record
+            record={currentrecord}
+            key={currentrecord._id}
+          />
+        );
+      });
+
   }
 
   // This following section will display the table with the records of individuals.
@@ -122,7 +78,7 @@ export default class RecordList extends Component {
       <div>
         <h3>Ticket List</h3>
         <div>
-        {!this.state.token && (
+          {!this.state.token && (
             <a
               className="btn btn--loginApp-link"
               href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
@@ -132,32 +88,27 @@ export default class RecordList extends Component {
               Login to Spotify
             </a>
           )}
-          {this.state.token && !this.state.no_data && (
-            <Player
-              item={this.state.item}
-              is_playing={this.state.is_playing}
-              progress_ms={this.state.progress_ms}
-            />
-          )}
-          {this.state.no_data && (
-            <p>
-              You need to be playing a song on Spotify, for something to appear here.
-            </p>
-          )}
-          </div>
+        </div>
 
-        <table className="table table-striped" style={{ marginTop: 20 }}>
-          <thead>
-            <tr>
-              <th>Month</th>
-              <th>Day</th>
-              <th>Band</th>
-              <th>Price</th>
-              <th>Sold Out</th>
-            </tr>
-          </thead>
-          <tbody>{this.recordList()}</tbody>
-        </table>
+        <div>
+          {this.state.token && (
+
+            <table className="table table-striped" style={{ marginTop: 20 }}>
+              <thead>
+                <tr>
+                  <th>Month</th>
+                  <th>Day</th>
+                  <th>Band</th>
+                  <th>Price</th>
+                  {/* <th>Sold Out</th> */}
+                  <th>Link</th>
+                </tr>
+              </thead>
+              <tbody>{this.recordList()}</tbody>
+
+            </table>
+          )}
+        </div>
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ const dbo = require("./db/conn");
 
 const request = require('request');
 const cheerio = require('cheerio');
+const axios = require('axios');
+
+var _token;
 
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -30,12 +33,14 @@ app.listen(port, () => {
   await new Promise(r => setTimeout(r, 2000));
 
   let db_connect = dbo.getDb();
+  let ticketsLinked = await addSpotifyLink(tickets);
 
   // comment out for testing
-  /* db_connect.collection("test_db").insertMany(tickets, function (err, res) {
+  db_connect.collection("test_db").insertMany(ticketsLinked, function (err, res) {
     if (err) throw err;
     console.log(res);
-  }); */
+  });
+
 
 })();
 
@@ -101,3 +106,86 @@ function extract_data() {
   });
 };
 
+
+
+
+async function addSpotifyLink(data) {
+
+  var client_id = '276aafa1475e4e5883df3bdf2899fc3a'; // Your client id
+  var client_secret = '4cbc7336bd9b481e804c516926f57126'; // Your secret
+
+  // your application requests authorization
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  var getAccessToken = await new Promise(function (resolve, reject) {
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        // use the access token to access the Spotify Web API
+        var token = body.access_token;
+        var options = {
+          url: 'https://api.spotify.com/v1/users/n54pdoqawgph9ftwztxe6k77a',
+          headers: {
+            'Authorization': 'Bearer ' + token
+          },
+          json: true
+        };
+        request.get(options, function (error, response, body) {
+          //console.log(body);
+          //console.log(token);
+          resolve(token);
+
+        });
+      }
+    });
+  });
+
+  for (const element of data) {
+
+    await new Promise(function (resolve, reject) {
+      axios({
+        url: 'https://api.spotify.com/v1/search',
+        method: 'GET',
+        headers: {
+          "Authorization": "Bearer " + getAccessToken,
+        },
+        params: {
+          "q": element.ticket_band,
+          "type": 'artist'
+        }
+
+      }).then(function (res) {
+
+        //console.log(res.data.artists.items[0].external_urls.spotify);
+        var ret;
+        try {
+          ret = res.data.artists.items[0].external_urls.spotify;
+        }
+        catch {
+          ret = " ";
+        }
+
+        element.link = ret;
+        resolve(ret);
+
+      }).catch(function (error) {
+
+        console.log("error");
+      });
+
+    });
+
+  }
+
+  return data;
+
+}
