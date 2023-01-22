db79aa15ceb1ddda4c5c6aa2afb8eec09c73077e
Devin-Bushey <devin.bushey.pdc@gmail.com>
2023-01-21 22:00:30 -0800

    Added template for swagger doc

---
+++ server/docs/basicInfo.js
@@ -0,0 +1,8 @@
+module.exports = {
+  openapi: "3.0.3", // present supported openapi version
+  info: {
+    title: "Record Shop API", // short title.
+    description: "In progress ...",
+    version: "0.0.0", // version number
+  },
+};
--- server/server.js
+++ server/server.js
@@ -6,12 +6,17 @@ const port = process.env.PORT || 5000;
 const cron = require("node-cron");
 let shell = require("shelljs");
 
+const swaggerUI = require("swagger-ui-express");
+const docs = require('./docs');
+
 const dbo = require("./db/conn");
 
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 app.use('/healthcheck', require('./routes/healthchecker'));
+app.use('/api', swaggerUI.serve, swaggerUI.setup(docs));
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
