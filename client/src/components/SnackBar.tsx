59ee4ae183bad96a525319de6e8950f149c65edc
Devin Bushey <59553292+devin-bushey@users.noreply.github.com>
2023-03-18 12:51:34 -0700

    Added sign up page (#35)

---
+++ client/src/components/SnackBar.tsx
@@ -0,0 +1,38 @@
+import { Alert, Snackbar } from '@mui/material';
+
+/**
+ * Type for displaying snackbar (aka toast message) after an operation.
+ */
+export interface SnackBarOptions {
+  showSnackbar: boolean;
+  setShowSnackbar: React.Dispatch<React.SetStateAction<boolean>>;
+  message: string;
+  isError: boolean;
+}
+
+/**
+ * Displays a snackbar.
+ * @param showSnackbar - boolean indicating if the snackbar should be displayed.
+ * @param setShowSnackbar - setState variable to set if the snackbar should be displayed.
+ * @param message - string containing the message.
+ * @param isError - boolean indicating if the snackbar expresses an error.
+ */
+export const SnackBar = ({ showSnackbar, setShowSnackbar, message, isError }: SnackBarOptions) => {
+  const vertical = 'top';
+  const horizontal = 'right';
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setShowSnackbar(false);
+  };
+
+  return (
+    <Snackbar open={showSnackbar} autoHideDuration={6000} onClose={handleClose} anchorOrigin={{ vertical, horizontal }}>
+      <Alert onClose={handleClose} severity={isError ? 'error' : 'success'} sx={{ width: '100%' }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
