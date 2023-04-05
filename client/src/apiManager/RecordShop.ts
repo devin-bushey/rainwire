ec2ecb21fc5cd920841238331d810315cf5242b0
Devin Bushey <59553292+devin-bushey@users.noreply.github.com>
2023-04-05 11:54:22 -0700

    Sort tickets on the frontend (#37)
    * Sort tickets on frontend
    
    * Clean up code

--- client/src/apiManager/RecordShop.ts
+++ client/src/apiManager/RecordShop.ts
@@ -1,9 +1,29 @@
 import axios from 'axios';
+import moment from 'moment';
 
 export const GetTickets = async (city: string) => {
   return axios
     .get(import.meta.env.VITE_SITE_URL_DB + city + '/')
     .then((response) => {
+      response.data.sort((a: any, b: any) => {
+        let a_date = a.date;
+        let b_date = b.date;
+
+        if (a_date.length < 10) {
+          a_date = moment(a_date, 'MMM DD').format('MM-DD');
+          a_date = moment().format('YYYY') + '-' + a_date;
+        }
+
+        if (b_date.length < 10) {
+          b_date = moment(a_date, 'MMM DD').format('MM-DD');
+          b_date = moment().format('YYYY') + '-' + a_date;
+        }
+
+        const dateA = new Date(a_date);
+        const dateB = new Date(b_date);
+        return dateA.getTime() - dateB.getTime();
+      });
+
       return response.data;
     })
     .catch((error) => {
