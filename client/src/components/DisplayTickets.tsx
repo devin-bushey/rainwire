bc680b1820906dea60b3eb3a6ea5378191e40cb3
Devin-Bushey <devin.m.bushey@gmail.com>
2023-06-01 21:12:04 -0700

    test

--- client/src/components/DisplayTickets.tsx
+++ client/src/components/DisplayTickets.tsx
@@ -24,8 +24,8 @@ import { AUTH_ENDPOINT, BASE_REDIRECT_URI, CLIENT_ID, SCOPES } from '../constant
 import { Origin } from './Origin';
 import { Settings } from './Settings';
 import { TicketContainer } from './TicketContainer';
-import { groupByLocation } from '../utils/groupByLocation';
 import './styles/ClickMe.css';
+import { InAppModal } from './InAppModal';
 
 export const DisplayTickets = (data: any) => {
   const { token, spotifyInfo } = useSpotifyAuth();
@@ -175,8 +175,7 @@ export const DisplayTickets = (data: any) => {
       handleOpen();
       return true;
     }
-    handleOpen();
-    //handleRedirectToAuth();
+    handleRedirectToAuth();
     return false;
   };
 
@@ -292,66 +291,7 @@ export const DisplayTickets = (data: any) => {
         )}
       </Box>
 
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: '90%',
-            maxWidth: 400,
-            bgcolor: 'background.paper',
-            border: '2px solid #000',
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ maxWidth: '70%' }}>
-            Looks like you&apos;re using an in-app browser.
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            If you use{' '}
-            <Box fontWeight="bold" display="inline">
-              Google to sign in
-            </Box>{' '}
-            with Spotify then please open Record Shop with Chrome, Safari, Firefox, etc.
-          </Typography>
-
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              gap: '10px',
-              alignItems: 'center',
-              marginTop: '10px',
-            }}
-          >
-            <Typography sx={{ paddingBottom: '0px' }}>https://recordshop.cool</Typography>
-            <Button variant="outlined" onClick={() => navigator.clipboard.writeText('recordshop.cool')}>
-              Copy
-            </Button>
-          </Box>
-
-          <Box sx={{ display: 'flex', justifyContent: 'center', gap: '10px', alignItems: 'center', marginTop: '10px' }}>
-            <Button
-              onClick={handleRedirectToAuth}
-              variant="contained"
-              color="secondary"
-              //className="btn--click-me"
-              sx={{ width: '310px', marginTop: '12px', justifyContent: 'center' }}
-            >
-              <img src={spotifyIcon} alt="spotify_logo" width="20px" height="20px" style={{ marginRight: '8px' }} />
-              <Typography sx={{ paddingBottom: 0 }}>Continue</Typography>
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
+      <InAppModal open={open} handleClose={handleClose} handleRedirectToAuth={handleRedirectToAuth} />
     </>
   );
 };
--- client/src/components/MainPage.tsx
+++ client/src/components/MainPage.tsx
@@ -1,14 +1,38 @@
 import { Box, Container } from '@mui/material';
 import Button from '@mui/material/Button/Button';
 import Typography from '@mui/material/Typography';
-import { memo, useEffect } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { COLOURS } from '../theme/AppStyles';
 import spotifyLogo from '../spotifyLogos/Spotify_Icon_RGB_Black.png';
 import { useNavigate } from 'react-router-dom';
 import { AUTH_ENDPOINT, BASE_REDIRECT_URI, CLIENT_ID, SCOPES } from '../constants/auth';
+import { InAppModal } from './InAppModal';
 
 const MainPage = memo(() => {
   const navigate = useNavigate();
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const isInAppBrowser = () => {
+    console.log(navigator.userAgent);
+    // check if this react app is open within Instagram, LinkedIn, or Facebook's in-app browser
+    if (navigator.userAgent.match(/FBAN|FBAV|Instagram|LinkedIn|Messenger/i)) {
+      // in-app browser detected
+      handleOpen();
+      return true;
+    }
+    handleOpen();
+    //handleRedirectToAuth();
+    return false;
+  };
+
+  const handleRedirectToAuth = () => {
+    location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${BASE_REDIRECT_URI}&scope=${SCOPES.join(
+      '%20',
+    )}&response_type=token&show_dialog=true`;
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -39,9 +63,10 @@ const MainPage = memo(() => {
 
       <Box display="flex" flexDirection="column">
         <Button
-          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${BASE_REDIRECT_URI}&scope=${SCOPES.join(
-            '%20',
-          )}&response_type=token&show_dialog=true`}
+          onClick={isInAppBrowser}
+          // href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${BASE_REDIRECT_URI}&scope=${SCOPES.join(
+          //   '%20',
+          // )}&response_type=token&show_dialog=true`}
           variant="contained"
           color="secondary"
           sx={{ marginTop: '24px', marginBottom: '24px', padding: '8px 16px', maxWidth: '222px' }}
@@ -78,6 +103,8 @@ const MainPage = memo(() => {
       >
         <Typography sx={{ paddingTop: '12px' }}>To get started, sign in with Spotify.</Typography>
       </Box>
+
+      <InAppModal open={open} handleClose={handleClose} handleRedirectToAuth={handleRedirectToAuth} />
     </Container>
   );
 });
