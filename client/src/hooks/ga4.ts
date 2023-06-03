8fc954a35a564b85e89576cabe05c4830debff34
Devin Bushey <59553292+devin-bushey@users.noreply.github.com>
2023-06-03 10:50:11 -0700

    Added google analytics (#77)

---
+++ client/src/hooks/ga4.ts
@@ -0,0 +1,17 @@
+import ga4 from 'react-ga4';
+
+const TRACKING_ID = import.meta.env.VITE_GOOGLE_ANALYTICS;
+
+export const init = () => ga4.initialize(TRACKING_ID);
+
+export const sendEvent = (name: string) =>
+  ga4.event('screen_view', {
+    app_name: 'myApp',
+    screen_name: name,
+  });
+
+export const sendPageview = (path: string) =>
+  ga4.send({
+    hitType: 'pageview',
+    page: path,
+  });
