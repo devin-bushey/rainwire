b4e29285283403929162076685e85899ba7b32ba
Devin Bushey <59553292+devin-bushey@users.noreply.github.com>
2023-06-10 13:39:02 -0700

    Rifflandia dedicated page POC (#80)
    * POC for rifflandia dedicated page
    
    * Fixed responsive design

---
+++ client/src/components/Rifflandia/LoadingRifflandia.tsx
@@ -0,0 +1,26 @@
+import { Container, Typography } from '@mui/material';
+import '../styles/Loading.css';
+import { RIFFLANDIA_COLOURS } from './colours';
+
+export const LoadingRifflandia = () => {
+  return (
+    <Container sx={{ paddingTop: '100px', textAlign: 'center' }}>
+      <Typography variant="h5" sx={{ color: 'black', textAlign: 'center', marginBottom: '8px' }}>
+        L O A D I N G
+      </Typography>
+      <div className="loader">
+        <ul className="wine_ul">
+          <li className="wine_li wine_li_1"></li>
+          <li className="wine_li wine_li_2"></li>
+          <li className="wine_li wine_li_3"></li>
+        </ul>
+        <div className="wineglass wine_left">
+          <div className="wine_top"></div>
+        </div>
+        <div className="wineglass wine_right">
+          <div className="wine_top"></div>
+        </div>
+      </div>
+    </Container>
+  );
+};
---
+++ client/src/components/Rifflandia/colours.ts
@@ -0,0 +1,7 @@
+export const RIFFLANDIA_COLOURS = {
+  background: '#f6edb5',
+  pink: '#cd267f',
+  orange: '#f7c711',
+  blue: '#00adef',
+  green: '#70be44',
+};
