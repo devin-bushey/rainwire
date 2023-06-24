ca7eb4609f1e2aae1ba0264f6aec69fd9a001e0d
Devin-Bushey <devin.m.bushey@gmail.com>
2023-06-24 10:02:55 -0700

    Added vite plugin for ga4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,14 +3,30 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
-import path from 'path';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [eslint({ cache: false }), react(), viteTsconfigPaths(), svgrPlugin()],
+    plugins: [
+      eslint({ cache: false }),
+      react(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+      VitePluginRadar({
+        // Google Analytics tag injection
+        analytics: {
+          id: 'G-WK0NXL9P3R',
+        },
+        gtm: [
+          {
+            id: 'GTM-NCVHZ37',
+          },
+        ],
+      }),
+    ],
     server: {
       host: true,
       port: parseInt(process.env.VITE_PORT || '3000'),
