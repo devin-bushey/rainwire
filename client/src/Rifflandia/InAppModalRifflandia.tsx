1135a7547d07b128765b6550e534dec593366a19
Devin-Bushey <devin.m.bushey@gmail.com>
2023-07-06 20:34:19 -0700

    Updated in app modal for Rifflandia

--- client/src/Rifflandia/InAppModalRifflandia.tsx
+++ client/src/Rifflandia/InAppModalRifflandia.tsx
@@ -1,5 +1,9 @@
 import { Modal, Box, Typography, Button } from '@mui/material';
 import spotifyIcon from '../spotifyLogos/Spotify_Icon_RGB_Black.png';
+import { RIFFLANDIA_COLOURS } from './constants/colours';
+import { useContext } from 'react';
+import { SnackBarContext } from '../App';
+import copy from '../assets/images/copy-solid.svg';
 
 export const InAppModalRifflandia = ({
   open,
@@ -10,6 +14,17 @@ export const InAppModalRifflandia = ({
   handleClose: any;
   handleRedirectToAuth: any;
 }) => {
+  const snackBar = useContext(SnackBarContext);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('recordshop.cool/rifflandia');
+    snackBar.setSnackBar({
+      showSnackbar: true,
+      setShowSnackbar: () => true,
+      message: 'Copied! Please paste URL in a new browser window',
+      isError: false,
+    });
+  };
   return (
     <Modal
       open={open}
@@ -25,29 +40,35 @@ export const InAppModalRifflandia = ({
           transform: 'translate(-50%, -50%)',
           width: '90%',
           maxWidth: 400,
-          bgcolor: 'background.paper',
+          backgroundColor: RIFFLANDIA_COLOURS.fill_pale_green,
           border: '2px solid #000',
           boxShadow: 24,
           p: 4,
         }}
       >
-        <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ maxWidth: '70%' }}>
+        <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ maxWidth: '70%', fontWeight: '700' }}>
           Looks like you&apos;re using an in-app browser.
         </Typography>
         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          If you have trouble signing in with Spotify then please try again by opening Record Shop with Chrome, Safari,
+          If you have trouble signing in with Spotify then please try again by opening Record Shop using Chrome, Safari,
           Firefox, etc.
         </Typography>
         <Box sx={{ display: 'flex', justifyContent: 'center', gap: '10px', alignItems: 'center' }}>
           <Button
             onClick={handleRedirectToAuth}
             variant="contained"
-            color="secondary"
-            //className="btn--click-me"
-            sx={{ width: '100%', marginTop: '12px', justifyContent: 'center' }}
+            sx={{
+              width: '100%',
+              marginTop: '12px',
+              justifyContent: 'center',
+              backgroundColor: RIFFLANDIA_COLOURS.light_blue,
+              ':hover': {
+                backgroundColor: RIFFLANDIA_COLOURS.dark_blue,
+              },
+            }}
           >
             <img src={spotifyIcon} alt="spotify_logo" width="20px" height="20px" style={{ marginRight: '8px' }} />
-            <Typography sx={{ paddingBottom: 0 }}>Continue</Typography>
+            <Typography sx={{ paddingBottom: 0, color: 'black' }}>Continue</Typography>
           </Button>
         </Box>
 
@@ -60,19 +81,9 @@ export const InAppModalRifflandia = ({
             marginTop: '20px',
           }}
         >
-          <Typography sx={{ paddingBottom: '0px' }}>https://recordshop.cool/rifflandia</Typography>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '10px',
-            alignItems: 'center',
-            marginTop: '16px',
-          }}
-        >
-          <Button variant="outlined" onClick={() => navigator.clipboard.writeText('recordshop.cool/rifflandia')}>
-            Copy
+          <Typography sx={{ paddingBottom: '0px', fontSize: '14px' }}>https://recordshop.cool/rifflandia</Typography>
+          <Button onClick={handleCopy} sx={{ minWidth: '20px' }}>
+            <img src={copy} alt="copy" height="20px" />
           </Button>
         </Box>
       </Box>
