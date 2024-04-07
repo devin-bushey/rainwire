688dbea3b63de86c8e78dfd489abc6b1d06fbe63
Devin Bushey <59553292+devin-bushey@users.noreply.github.com>
2024-04-06 22:38:57 -0700

    Remove cache for victoria (#197)

--- backend/src/controller/victoriaController.ts
+++ backend/src/controller/victoriaController.ts
@@ -2,8 +2,6 @@ import express from "express";
 export const victoriaRouter = express.Router();
 
 import { CreateNewPlaylist } from "../helpers/createPlaylist";
-import { Cities } from "../enums/Cities";
-import { cachedGigs } from "../cache/gigsCache";
 import { Gig } from "../types/Gig";
 import { Collection } from "mongodb";
 import { connectToDatabase } from "../database/connectToDatabase";
@@ -11,21 +9,11 @@ import { connectToDatabase } from "../database/connectToDatabase";
 victoriaRouter.route("/artists").get(async (req, response) => {
   const { city } = req.query;
 
-  if (city === Cities.Victoria_2024 && cachedGigs.cachedGigsVictoria) {
-    response.json(cachedGigs.cachedGigsVictoria);
-  } else {
-    if (city === Cities.Victoria_2024) console.log("cache not found for /victoria: ", cachedGigs.cachedGigsVictoria);
+  const db_connect = await connectToDatabase();
+  const collection: Collection<Gig> = db_connect.collection(`${city}`);
+  const gigs: Gig[] = await collection.find({}).toArray();
 
-    const db_connect = await connectToDatabase();
-    const collection: Collection<Gig> = db_connect.collection(`${city}`);
-    const gigs: Gig[] = await collection.find({}).toArray();
-
-    // Save the fetched data to cache
-    if (city === Cities.Victoria_2024) {
-      cachedGigs.cachedGigsVictoria = gigs;
-    }
-    response.json(gigs);
-  }
+  response.json(gigs);
 });
 
 victoriaRouter.route("/create").post(async (req, response) => {
