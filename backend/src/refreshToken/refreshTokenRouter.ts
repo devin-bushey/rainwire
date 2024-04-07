c73bef64d82499f07eb695566153e01ff6a7e09d
Devin-Bushey <devin.m.bushey@gmail.com>
2024-04-06 21:58:15 -0700

    Added github action to update victoria database

---
+++ backend/src/refreshToken/refreshTokenRouter.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosRequestConfig } from "axios";
+import express, { Request, Response } from "express";
+
+export const refreshTokenRouter = express.Router();
+
+const CLIENT_ID = process.env.SP_CLIENT_ID as string;
+const CLIENT_SECRET = process.env.SP_CLIENT_S as string;
+const REDIRECT_URI = "http://localhost:5000/callback";
+const SCOPE = ["playlist-modify-public"];
+
+refreshTokenRouter.get("/login", (request: Request, response: Response) => {
+  const redirect_url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${SCOPE.join("%20")}&state=123456&redirect_uri=${REDIRECT_URI}&prompt=consent`;
+  response.redirect(redirect_url);
+});
+
+refreshTokenRouter.get("/callback", async (request: Request, response: Response) => {
+  const code = request.query["code"] as string;
+  const tokenRequestConfig: AxiosRequestConfig = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    auth: {
+      username: CLIENT_ID,
+      password: CLIENT_SECRET,
+    },
+  };
+
+  try {
+    const resp1 = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      new URLSearchParams({
+        grant_type: "authorization_code",
+        redirect_uri: REDIRECT_URI,
+        code: code,
+      }),
+      tokenRequestConfig,
+    );
+
+    const resp2 = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: resp1.data.refresh_token,
+      }),
+      tokenRequestConfig,
+    );
+
+    response.json([resp1.data, resp2.data]);
+  } catch (error) {
+    console.error("Error occurred during token refresh:");
+    response.status(500).json({ error: "Failed to refresh token" });
+  }
+});
