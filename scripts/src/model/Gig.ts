399dfbffe5c01b2f51ff0507f532844be1dc364f
Devin-Bushey <devin.m.bushey@gmail.com>
2024-04-07 11:24:12 -0700

    Clean up github action pt1

--- scripts/src/model/Gig.ts
+++ scripts/src/model/Gig.ts
@@ -5,7 +5,7 @@ export type UnsavedGig = Omit<Gig, "_id">;
 export interface Gig {
   _id: string;
   artist: Artist;
-  date: Date;
+  date: Date | string;
   venue: string;
   isMissing?: boolean;
   popularity?: number;
