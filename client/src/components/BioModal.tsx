69354e307be827ac59fa422d3519182775eabc8c
Devin-Bushey <devin.m.bushey@gmail.com>
2024-04-21 20:57:08 -0700

    Add link to artists website and instagram to bio

--- client/src/components/BioModal.tsx
+++ client/src/components/BioModal.tsx
@@ -1,17 +1,17 @@
-import { Modal, Box, Typography } from "@mui/material";
+import { Modal, Box, Typography, Link } from "@mui/material";
 import { Artist } from "../types/Gig";
 import { PageClassName } from "../theme/AppStyles";
 
 type BioModalProps = {
-  artistBio?: Artist;
-  setArtistBio: (artistBio?: Artist) => void;
+  artist?: Artist;
+  setArtist: (artist?: Artist) => void;
   pageClassName?: PageClassName;
 };
 
 const DEFAULT_BACKGROUND_COLOUR = "#2596be";
 
-export const BioModal = ({ artistBio, setArtistBio, pageClassName }: BioModalProps) => (
-  <Modal open={!!artistBio} onClose={() => setArtistBio(undefined)} disableAutoFocus={true} className={pageClassName}>
+export const BioModal = ({ artist, setArtist, pageClassName }: BioModalProps) => (
+  <Modal open={!!artist} onClose={() => setArtist(undefined)} disableAutoFocus={true} className={pageClassName}>
     <Box
       className="scroll-style artist-bio"
       sx={{
@@ -33,10 +33,40 @@ export const BioModal = ({ artistBio, setArtistBio, pageClassName }: BioModalPro
     >
       <Box>
         <Typography fontSize="24px" fontWeight="bold">
-          {artistBio?.name}
+          {artist?.name}
         </Typography>
-        <Typography>{artistBio?.bio}</Typography>
+        <SocialLinks artist={artist} />
+        <Typography>{artist?.bio}</Typography>
       </Box>
     </Box>
   </Modal>
 );
+
+const SocialLinks = ({ artist }: { artist?: Artist }) => {
+  if (!artist?.website && !artist?.instagram) return;
+
+  return (
+    <Box sx={{ marginBottom: "12px" }}>
+      <Website website={artist?.website} />
+      <Instagram instagram={artist?.instagram} />
+    </Box>
+  );
+};
+
+const Website = ({ website }: { website?: string }) => {
+  if (!website) return;
+  return (
+    <Link href={website} underline="hover" rel="noopener" target="_blank" sx={{ marginRight: "12px" }}>
+      Website
+    </Link>
+  );
+};
+
+const Instagram = ({ instagram }: { instagram?: string }) => {
+  if (!instagram) return;
+  return (
+    <Link href={instagram} rel="noopener" target="_blank" underline="hover">
+      Instagram
+    </Link>
+  );
+};
