f8f109e8a9592c176a0b9159b0184c38c145eaa6
Devin-Bushey <devin.m.bushey@gmail.com>
2024-05-04 14:12:10 -0700

    Add script for updating existing playlist

--- backend/src/helpers/spotifyPlaylistHelpers.ts
+++ backend/src/helpers/spotifyPlaylistHelpers.ts
@@ -34,22 +34,33 @@ export const getPlaylist = async ({
 };
 
 export const getPlaylistItems = async ({ token, playlistId }: { token: any; playlistId: any }) => {
-  try {
-    const response = await get({
-      url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const tracks = [];
-    for (const item of response.data.items) {
-      tracks.push(item.track);
+  const tracks = [];
+
+  const limit = 50;
+  let offset = 0;
+  let response;
+
+  do {
+    try {
+      response = await get({
+        url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          offset,
+          limit,
+        },
+      });
+
+      tracks.push(...response.data.items.map((item: any) => item.track));
+      offset += limit;
+    } catch (error) {
+      console.log("Error at getPlaylistItems:", error);
     }
-    return tracks;
-  } catch (error) {
-    console.log("Error at checkIfPlaylistExists:", error);
-  }
-  return [];
+  } while (response?.data?.total > offset);
+
+  return tracks;
 };
 
 export const removePlaylistItems = async ({
@@ -59,19 +70,31 @@ export const removePlaylistItems = async ({
 }: {
   token: string;
   playlistId: string;
-  tracks: string[];
+  tracks: any[];
 }) => {
-  try {
-    await delete_({
-      url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      data: { tracks: tracks.map((track: any) => ({ uri: track.uri })) },
-    });
-  } catch (error) {
-    console.log("Error at removePlaylistItems:", error);
-  }
+  let deletedTrackCount = 0;
+
+  do {
+    const batchedTracks = tracks.splice(0, 100);
+
+    try {
+      await delete_({
+        url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: { tracks: batchedTracks.map((track: any) => ({ uri: track.uri })) },
+      });
+    } catch (error) {
+      console.log("Error at removePlaylistItems:", error);
+    }
+
+    deletedTrackCount += batchedTracks.length;
+  } while (tracks.length);
+
+  console.log(`Deleted ${deletedTrackCount} tracks from playlist.`);
+
+  return deletedTrackCount;
 };
 
 export const addPlaylistItems = async ({
@@ -83,17 +106,29 @@ export const addPlaylistItems = async ({
   playlistId: any;
   tracks: any[];
 }) => {
-  try {
-    await post({
-      url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      data: {
-        uris: tracks,
-      },
-    });
-  } catch (error) {
-    console.log("Error at addPlaylistItems:", error);
-  }
+  let addedTracksCount = 0;
+  do {
+    const batchedGigs = tracks.splice(0, 75);
+    const batchedTracks = batchedGigs.map((gig) => gig.artist.topTracks[0]);
+
+    try {
+      await post({
+        url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: {
+          uris: batchedTracks,
+        },
+      });
+    } catch (error) {
+      console.log("Error at addPlaylistItems:", error);
+    }
+
+    addedTracksCount += batchedTracks.length;
+  } while (tracks.length);
+
+  console.log(`Added ${addedTracksCount} tracks to playlist.`);
+
+  return addedTracksCount;
 };
