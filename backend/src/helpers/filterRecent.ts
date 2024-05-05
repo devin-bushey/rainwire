69647980ca07126ee38a77d3d6bf230df3086a04
Devin Bushey <59553292+devin-bushey@users.noreply.github.com>
2024-05-05 10:37:34 -0700

    Change filter to be one week ago instead of two (#258)

--- backend/src/helpers/filterRecent.ts
+++ backend/src/helpers/filterRecent.ts
@@ -2,8 +2,8 @@ import { Gig } from "../types/Gig";
 
 export const filterRecent = (gigs: Gig[]) => {
   return gigs.filter((gig: Gig) => {
-    const twoWeeksAgo: Date = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000);
+    const oneWeekAgo: Date = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
     const gigDate = new Date(gig.date);
-    return gigDate > twoWeeksAgo;
+    return gigDate > oneWeekAgo;
   });
 };
--- client/src/utils/sorter.ts
+++ client/src/utils/sorter.ts
@@ -32,8 +32,8 @@ export const sortDataByDateAndOrder = (data: any) => {
 
 export const filterRecent = (data: any) => {
   return data.filter((ticket: any) => {
-    const twoWeeksAgo: Date = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000);
+    const oneWeekAgo: Date = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
     const ticketDate = new Date(ticket.date);
-    return ticketDate > twoWeeksAgo;
+    return ticketDate > oneWeekAgo;
   });
 };
